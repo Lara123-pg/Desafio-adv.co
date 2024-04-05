f1268e85e5fd6ffe3af7692d45d25b06f00fd3e5
lara123-pg <lamorimalvescavalcante@gmail.com>
2024-04-04 21:39:12 -0300

    feat:header

---
+++ src/Screens/Categories.tsx
@@ -0,0 +1,24 @@
+export function Categories() {
+    return (
+        <div>
+            <header>
+                <h1>What do you need designed?</h1>
+
+                <nav>
+                    <ul>
+                        <li>Pesquisar</li>
+                        <li>Logo & identity</li>
+                        <li>Web & app design</li>
+                        <li>Business & adversiting</li>
+                        <li>Clothing & merchandise</li>
+                        <li>Art & illustration</li>
+                        <li>Packaging & label</li>
+                        <li>Book & magazine</li>
+                    </ul>
+                </nav>
+            </header>
+
+            <main></main>
+        </div>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 
+import { ChakraProvider } from '@chakra-ui/react'
+import { GlobalStyles } from './styles/global.tsx';
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  brand: {
+    titleOptions: 'rgb(201, 197, 190)',
+    menu: 'rgb(232, 230, 227)',
+    mainOptions: 'rgb(69, 223, 254)',
+    descriptionOptions: 'rgb(175, 169, 158)'
+  },
+}
+
+const theme = extendTheme({ colors })
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <GlobalStyles/>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
